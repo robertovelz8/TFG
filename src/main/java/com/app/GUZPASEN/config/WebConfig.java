29955829597689559a0a6d91d01b28192b50839b
robertovelz8 <rvelvaz834@g.educaand.es>
2025-06-06 09:13:45 +0200

    Adaptación a AWS

--- src/main/java/com/app/GUZPASEN/config/WebConfig.java
+++ src/main/java/com/app/GUZPASEN/config/WebConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig {
 
@@ -14,7 +16,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
